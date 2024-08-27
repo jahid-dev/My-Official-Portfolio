da9171d770c897d4d4aa9d7431002448ab7f2bb4
jahid-dev <mrjahidhassan0@gmail.com>
2024-08-27 16:54:22 +0600

    clsx and tailwind merge setup complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import {clsx} from "clsx";
+import { cn } from "@/lib/utils";
 export default function Home() {
   return (
    <div>
-      <h1 className="text-9xl font-bold text-red-500">Hello Portfolio</h1>
+      <h1 className={cn("text-8xl","italic","text-yellow-400", true && "underline")}>Hello</h1>
    </div>
   );
 }
