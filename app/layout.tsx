ddd415d7486b5355e58b8be861a1eba3306ab815
jahid-dev <mrjahidhassan0@gmail.com>
2024-08-28 22:13:53 +0600

    grain background added with reusable components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Bricolage_Grotesque, Oswald } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import GrainEffect from "@/components/visualEffects/grain-effect";
 
 const MainFont = Bricolage_Grotesque({ subsets: ["latin"] });
 const OswaldFont = Oswald({ subsets: ["latin"], variable: "--font-oswald "});
@@ -23,7 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(MainFont.className, OswaldFont.variable, PixelFont.variable)}>{children}</body>
+      <body className={cn(
+            MainFont.className,
+            OswaldFont.variable,
+            PixelFont.variable
+          )}>
+            <GrainEffect/>
+            {children}
+            </body>
     </html>
   );
 }
