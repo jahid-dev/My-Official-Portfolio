fb72038ede4fc1e44d67863e47f888c03c4db1d0
jahid-dev <mrjahidhassan0@gmail.com>
2025-10-10 22:03:01 +0600

    water effects added and fixed

---
+++ components/visualEffects/react-water-wave.d.ts
@@ -0,0 +1,16 @@
+"use client"
+declare module 'react-water-wave' {
+    import { FC } from 'react';
+  
+    interface WaterWaveProps {
+      imageUrl: string;
+      dropRadius?: number;
+      resolution?: number;
+      perturbance?: number;
+      children?: React.ReactNode;
+    }
+  
+    const WaterWave: FC<WaterWaveProps>;
+    export default WaterWave;
+  }
+  
