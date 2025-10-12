7dc93304e76d7f4ae3fa8ecbed980fc2c12fec8f
jahid-dev <mrjahidhassan0@gmail.com>
2025-10-12 22:07:35 +0600

    profil photo,timezone,scroll added

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@ import Button from "@/components/ui/button";
 import Card from "@/components/ui/card";
 import WaterWaveWrapper from "@/components/visualEffects/water-wave-wrapper.d";
 import { GoHomeFill } from "react-icons/go";
-import { FaUser } from "react-icons/fa";
+import { FaArrowRight, FaUser } from "react-icons/fa";
 import Input from "@/components/ui/input";
 import TextArea from "@/components/ui/text-area";
+import Profile from "@/components/ui/profile";
+import FancyButton from "@/components/ui/fancy-button";
+import { FaA } from "react-icons/fa6";
+import LiveClock from "@/components/ui/live-clock";
+import MagneticWrapper from "@/components/visualEffects/magnetic-wrapper";
+import ScrollDown from "@/components/ui/scroll-down";
 
 export default function Home() {
   return (
@@ -30,9 +36,17 @@ export default function Home() {
                       <Button link="https://www.google.com">Google</Button>
                     </div>
                     <Input type="text" placeholder="Full name" name="fullName"/>
-                    <Input type="text" placeholder="Full name" name="fullName" icon=<FaUser/> />
+                    <Input type="text" placeholder="Full name" name="fullName" icon={<FaUser/>} />
                     <TextArea  placeholder="Full name" name="fullName"  />
-                    <TextArea  placeholder="Full name" name="fullName" icon=<FaUser/> />
+                    <TextArea  placeholder="Full name" name="fullName" icon={<FaUser/> }/>
+                    <Profile/>
+                    <MagneticWrapper className="w-[350]">
+                      <FancyButton text="Contact Me" icon={<FaArrowRight/>} />
+                    </MagneticWrapper>
+                    <LiveClock timeZone = "Bangladesh/Dhaka"/>
+                    <MagneticWrapper>
+                      <ScrollDown/>
+                    </MagneticWrapper>
                   </Card>
                 </div>
             </div>}
